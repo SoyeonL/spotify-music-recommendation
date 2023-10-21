ee72853e720f36591dfce92d0c41e53588e44af9
SoyeonL <sylee932003@gmail.com>
2023-10-21 15:11:44 -0700

    fixed sx props for Box

--- client/src/pages/TrackDetail.js
+++ client/src/pages/TrackDetail.js
@@ -33,74 +33,65 @@ const TrackDetail = () => {
   }, [id]);
 
   return (
-    <div>
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: ["column", "row"],
-            justifyContent: ["", "center"],
-            alignItems: "center",
-            gap: ["10px", "50px"],
-            height: "50vh",
-          }}
-        >
-          {trackData && (
-            <>
-              <Box sx={{ mt: ["15px", 0] }}>
-                <img src={trackData.album.images[1].url} alt="" />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignSelf: ["", "flex-start"],
-                  // mt: ["0", "60px"],
-                  mt: "5%",
-                  gap: "15px",
-                  // pl: ["5%", 0],
-                  textAlign: ["center", ""],
-                }}
-              >
-                <Box sx={{ fontSize: [30, 70], fontWeight: "bold" }}>
-                  {trackData.name}
-                </Box>
-                <Box>
-                  <Link to={`/album-detail/${trackData.album.id}`}>
-                    {trackData.album.name}
-                  </Link>{" "}
-                  &bull; {trackData.album.release_date} &bull;{" "}
-                  <Link to={`/artist-detail/${trackData.artists[0].id}`}>
-                    {trackData.artists[0].name}
-                  </Link>
-                </Box>
-                <Box sx={{ margin: "auto" }}>
-                  {trackData.preview_url ? (
-                    <>
-                      <IconButton onClick={playSongHandler}>
-                        <PlayCircleIcon sx={{ fontSize: "150px" }} />
-                        {isPlaying && (
-                          <audio
-                            onEnded={stopSongHandler}
-                            autoPlay={true}
-                            src={trackData.preview_url}
-                          ></audio>
-                        )}
-                      </IconButton>
-                      <IconButton onClick={stopSongHandler}>
-                        <StopCircleIcon sx={{ fontSize: "150px" }} />
-                      </IconButton>
-                    </>
-                  ) : (
-                    <h1>Preview not available</h1>
-                  )}
-                </Box>
-              </Box>
-            </>
-          )}
-        </Box>
-      </Container>
-    </div>
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        flexDirection: ["column", "row"],
+        justifyContent: ["", "center"],
+        alignItems: "center",
+        gap: ["10px", "50px"],
+        height: "50vh",
+      }}
+    >
+      {trackData && (
+        <>
+          <Box sx={{ mt: ["15px", 0] }}>
+            <img src={trackData.album.images[1].url} alt="" />
+          </Box>
+          <Box
+            sx={{
+              gap: "15px",
+              textAlign: ["center", ""],
+            }}
+          >
+            <Box sx={{ fontSize: [30, 70], fontWeight: "bold" }}>
+              {trackData.name}
+            </Box>
+            <Box>
+              <Link to={`/album-detail/${trackData.album.id}`}>
+                {trackData.album.name}
+              </Link>{" "}
+              &bull; {trackData.album.release_date} &bull;{" "}
+              <Link to={`/artist-detail/${trackData.artists[0].id}`}>
+                {trackData.artists[0].name}
+              </Link>
+            </Box>
+            <Box sx={{ margin: "auto" }}>
+              {trackData.preview_url ? (
+                <>
+                  <IconButton onClick={playSongHandler}>
+                    <PlayCircleIcon sx={{ fontSize: "150px" }} />
+                    {isPlaying && (
+                      <audio
+                        onEnded={stopSongHandler}
+                        autoPlay={true}
+                        src={trackData.preview_url}
+                      ></audio>
+                    )}
+                  </IconButton>
+                  <IconButton onClick={stopSongHandler}>
+                    <StopCircleIcon sx={{ fontSize: "150px" }} />
+                  </IconButton>
+                </>
+              ) : (
+                <h1>Preview not available</h1>
+              )}
+            </Box>
+          </Box>
+        </>
+      )}
+    </Container>
   );
 };
 
